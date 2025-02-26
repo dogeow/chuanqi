0702ae24df3812bbbb4da36981f95c5887935d91
doge <doge@gmail.com>
2025-02-26 23:19:46 +0800

    新增物品

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GameSeeder::class,
             TestUserSeeder::class,
+            ItemsTableSeeder::class,
         ]);
     }
 }
